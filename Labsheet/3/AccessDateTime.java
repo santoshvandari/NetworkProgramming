4585f5a1737872f28aeca90df2bef150358c03b4
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-11 19:15:08 +0545

    Modified

---
+++ Labsheet/3/AccessDateTime.java
@@ -0,0 +1,20 @@
+// Program to Access daytime service from server using Socket.
+
+import java.net.*;
+import java.io.*;
+
+public class AccessDateTime {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("time.nist.gov" , 13);
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            int line;
+            while ((line = br.read()) != -1) {
+                System.out.print((char) line);
+            }
+            System.out.println();
+        }catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ Labsheet/4/RemoteAndLocalSocketAddress.java
@@ -0,0 +1,7 @@
+// Write a Program to Get Remote and Local Socket Address
+
+public class RemoteAndLocalSocketAddress {
+    public static void main(String[] args) {
+        
+    }
+}
