4e075103ae661bcdd01b879e20355eb5fd86a0d4
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-12 09:43:48 +0545

    Labsheet

--- Labsheet/7/SocketClient.java
+++ Labsheet/7/SocketClient.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
-public class ScoketClient {
+public class SocketClient {
     public static void main(String[] args) {
         try {
             Socket s = new Socket("localhost",6666);
