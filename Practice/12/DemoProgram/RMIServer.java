d87f0db53274c41183e7283811f0a12dc6abb0df
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-16 13:55:56 +0545

    Initial

--- Practice/12/DemoProgram/RMIServer.java
+++ Practice/12/DemoProgram/RMIServer.java
@@ -1,15 +1,12 @@
-// package Practice.12.DemoProgram;
-
-
-import java.rmi.*;
 import java.rmi.registry.*;
 public class RMIServer{
     public static void main(String args[]){
         try{
-            RMIInterfaceImplementation locobj = new RMIInterfaceImplementation();
+            RMIInterfaceImplementation obj = new RMIInterfaceImplementation();
             Registry rgsty = LocateRegistry.createRegistry(1888);
-            rgsty.rebind("localhost", locobj);
-            }catch(RemoteException re){System.out.println(re);
+            rgsty.rebind("localhost", obj);
+        }catch(Exception ex){
+            System.out.println(ex);
         }
     }
 }
